edd328038f62d1c03ac358d31d625f2cfc5a5920
Vishal Row Mysore <visrow@gmail.com>
2026-04-25 18:52:00 -0400

    Update base path for agentWorkBook repository

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/agentworkbook/',
+  base: '/agentWorkBook/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
